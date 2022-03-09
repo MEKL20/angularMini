c00aca7dda8140b104a619dff8f382267fa480fa
MEKL20 <edwinkhoirudin16@gmail.com>
2022-03-09 15:16:06 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ChildComponent } from './child/child.component';
+import { ChildValidationComponent } from './child-validation/child-validation.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    ChildComponent,
+    ChildValidationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/child-validation/child-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-validation',
+  templateUrl: './child-validation.component.html',
+  styleUrls: ['./child-validation.component.css']
+})
+export class ChildValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
